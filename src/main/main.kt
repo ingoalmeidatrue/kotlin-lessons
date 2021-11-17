a02d5a4bc6c57168d901ed10998e8f996836b440
Ingo Almeida <ingoalmeida@hotmail.com>
2021-11-17 15:03:26 -0300

    update gitignore

--- src/main/main.kt
+++ src/main/main.kt
@@ -4,37 +4,37 @@ import basics.Loops
 import basics.Variables
 
 fun main (args: Array<String>) {
-//    //VARIABLES CONCEPT
-//    println(Variables.immutableVariable)
-//    println(Variables.immutablePublicationLazyVariable)
-//    println(Variables.immutableSynchronizedLazyVariable)
-//    println(Variables.immutableOpenVariable)
-//    println(Variables.internalImmutableVariable)
-//    println(Variables.mutableVariable)
-//    println(Variables.CONSTANT_VALUE)
-//
-//    //CONDITIONALS CONCEPT
-//    val remoDescription = Conditionals.useIfs("remo")
-//    println(remoDescription)
-//    val tenisDescription = Conditionals.useIfs("tênis")
-//    println(tenisDescription)
-//    val ramonDescription = Conditionals.useSwitch("Ramon")
-//    println(ramonDescription)
-//    val victorDescription = Conditionals.useSwitch("Victor")
-//    println(victorDescription)
-//    val cunhaDescription = Conditionals.useSwitch("Cunha")
-//    println(cunhaDescription)
-//    Conditionals.useVerifierSwitch(1)
-//    Conditionals.useVerifierSwitch(14)
-//    Conditionals.useVerifierSwitch(20)
-//    Conditionals.useVerifierSwitch(25)
-//
-//    //BACKING PROPERTY AND FUNCTIONS
-//    val backingProperty = BackingProperty()
-//    println("Tamanho dos array de elementos: ${backingProperty.elementList.size}")
-//    backingProperty.addElements("Ingo Almeida")
-//    println("Tamanho dos array de elementos: ${backingProperty.elementList.size}")
-//    println("Primeira posição do array de elementos: ${backingProperty.elementList[0]}")
+    //VARIABLES CONCEPT
+    println(Variables.immutableVariable)
+    println(Variables.immutablePublicationLazyVariable)
+    println(Variables.immutableSynchronizedLazyVariable)
+    println(Variables.immutableOpenVariable)
+    println(Variables.internalImmutableVariable)
+    println(Variables.mutableVariable)
+    println(Variables.CONSTANT_VALUE)
+
+    //CONDITIONALS CONCEPT
+    val remoDescription = Conditionals.useIfs("remo")
+    println(remoDescription)
+    val tenisDescription = Conditionals.useIfs("tênis")
+    println(tenisDescription)
+    val ramonDescription = Conditionals.useSwitch("Ramon")
+    println(ramonDescription)
+    val victorDescription = Conditionals.useSwitch("Victor")
+    println(victorDescription)
+    val cunhaDescription = Conditionals.useSwitch("Cunha")
+    println(cunhaDescription)
+    Conditionals.useVerifierSwitch(1)
+    Conditionals.useVerifierSwitch(14)
+    Conditionals.useVerifierSwitch(20)
+    Conditionals.useVerifierSwitch(25)
+
+    //BACKING PROPERTY AND FUNCTIONS
+    val backingProperty = BackingProperty()
+    println("Tamanho dos array de elementos: ${backingProperty.elementList.size}")
+    backingProperty.addElements("Ingo Almeida")
+    println("Tamanho dos array de elementos: ${backingProperty.elementList.size}")
+    println("Primeira posição do array de elementos: ${backingProperty.elementList[0]}")
 
 
     //LOOPS CONCEPT
