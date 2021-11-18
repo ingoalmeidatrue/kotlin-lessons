c541e2229d096e3eacd32744a26922ecd6475732
Ingo Almeida <ingoalmeida@hotmail.com>
2021-11-18 11:57:15 -0300

    add comments

--- src/main/basics/Variables.kt
+++ src/main/basics/Variables.kt
@@ -15,9 +15,13 @@ object Variables {
     const val CONSTANT_VALUE = "Minha primeira constante"
 
     //LAZY CONCEPT
+    // Initializer function can be called several times on concurrent access to uninitialized Lazy instance value,
+    // but only the first returned value will be used as the value of Lazy instance.
     val immutablePublicationLazyVariable by lazy (LazyThreadSafetyMode.PUBLICATION) {
         val a = 1 + 1
         "Minha primeira variável imutável lazy com resultado de a = $a"
     }
+
+    //Locks are used to ensure that only a single thread can initialize the Lazy instance.
     val immutableSynchronizedLazyVariable by lazy (LazyThreadSafetyMode.SYNCHRONIZED) { "Minha primeira variável imutável lazy" }
 }
